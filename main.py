ccbb9520f1c70241e5c26aeae0450e431c79e32f
Michael Frank <michaelfrank2002@gmail.com>
2022-08-29 15:07:28 -0400

    beginnings of filesys API

--- main.py
+++ main.py
@@ -2,11 +2,28 @@
 
 # Author: Michael Frank
 
+# library imports
 import os
-import pathlib
 
-home = str(pathlib.Path.home())
+# local imports
+import filesys
+import shell
 
 
 if __name__ == "__main__":
-  print("test") 
+  # setup
+  if not os.path.exists(filesys.home):
+    os.mkdir(filesys.home)
+  
+  # important!
+  os.chdir(filesys.home)
+  
+  if len(os.listdir()) == 0:
+    print("Performing first-time setup...")
+    filesys.initialize()
+    print("Created category Tasks.")
+    
+  
+  shell.listen()
+    
+  
