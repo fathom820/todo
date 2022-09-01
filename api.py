07d9fa540ba558096da3a1cd12566e912bf47242
Michael Frank <michaelfrank2002@gmail.com>
2022-09-01 00:03:43 -0400

    test

---
+++ api.py
@@ -0,0 +1,10 @@
+# library imports
+# library imports
+import os
+
+# local imports
+
+def listen():
+  cmd = input("todo > ")
+  
+  #TODO: add stuff 
---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+# library imports
+import os
+
+# local imports
+import commands
+import api
+
+if __name__ == "__main__":
+  # commands.today()
+  
+  while(True):
+    api.listen()
